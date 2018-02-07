28d6a067095f906a250ef4a3c9867b6def583f5b
Jan Lopata <jan.lopata@rohlik.cz>
2018-02-07 19:19:49 +0100

    Convert mp3 to ogg

--- audio2hdf5/importer.py
+++ audio2hdf5/importer.py
@@ -1,13 +1,16 @@
 import os
+import subprocess as sp
 
+FFMPEG_BIN = "ffmpeg"
 
 """
 Class for importing mp3 audio to hdf5 format using
 - preprocessing (conversion to ogg format)
 - import ogg to python
 """
-class Importer:
 
+
+class Importer:
     extension_mp3 = ".mp3"
     extension_ogg = ".ogg"
 
@@ -23,6 +26,16 @@ class Importer:
     def get_ogg_files(self):
         return self.get_files_with_extension(self.extension_ogg)
 
-    def convert_mp3_to_ogg(self, filename):
-        pass
-
+    def convert_mp3_to_ogg(self, filename: str):
+        """
+            converts given filename from mp3 to ogg file if ogg does not already exists
+            this function is definitely NOT SAFE
+        """
+        mp3_path = os.path.join(self.directory, filename)
+        ogg_path = mp3_path.replace(self.extension_mp3, self.extension_ogg)
+        if os.path.isfile(ogg_path):
+            # already done
+            return
+        command = [FFMPEG_BIN, '-i', mp3_path, ogg_path]
+        pipe = sp.Popen(command, shell=False, stdout=sp.PIPE)
+        pipe.wait()
