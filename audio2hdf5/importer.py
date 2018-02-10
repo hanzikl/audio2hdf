dd0c6dd5e07539dabe5e7a6713a41d94a8ffc0b2
Jan Lopata <jan.lopata@rohlik.cz>
2018-02-10 18:13:10 +0100

    Initializing data mapping

--- audio2hdf5/importer.py
+++ audio2hdf5/importer.py
@@ -16,8 +16,19 @@ class Importer:
     extension_mp3 = ".mp3"
     extension_ogg = ".ogg"
 
-    def __init__(self, directory):
+    def __init__(self, directory, cutinterval=44100, overlap=0):
+        """
+        Init importer on given directory
+        :param directory:
+        :param cutinterval: length of one sample
+        :param overlap: size of overlapping
+        """
         self.directory = directory
+        self.data_mapping = None
+
+        assert cutinterval > overlap, "Cutinterval must be longer than overlap"
+        self.cut_interval = cutinterval
+        self.overlap = overlap
 
     def get_files_with_extension(self, extension):
         return [file_name for file_name in os.listdir(self.directory) if file_name.endswith(extension)]
@@ -46,9 +57,34 @@ class Importer:
         filepath = os.path.join(self.directory, filename)
         data, sample_rate = soundfile.read(filepath)
 
-        return data
+        return data, sample_rate
 
-    def parse_index(self, filename:str):
-        index = int(re.findall(r'\d+\.mp3$', filename)[0].replace('.mp3', ''))
+    def parse_index(self, filename: str):
+        index = int(re.findall(r'\d+\.ogg$', filename)[0].replace('.ogg', ''))
         return index
 
+    def is_initialized(self):
+        return self.data_mapping is not None
+
+    def initialize(self):
+        data_mapping = []
+        for mp3_file in self.get_mp3_files():
+            self.convert_mp3_to_ogg(mp3_file)
+
+        sample_rate_check = None
+        for ogg_file in self.get_ogg_files():
+            data, sample_rate = self.read_data_from_ogg(ogg_file)
+            if sample_rate_check is not None:
+                if sample_rate != sample_rate_check:
+                    print("Skipping file {} due sample rate mismatch (base :{}, current: {})"
+                          .format(ogg_file, sample_rate_check, sample_rate))
+            else:
+                sample_rate_check = sample_rate
+
+            audio_file_index = self.parse_index(ogg_file)
+            # count samples in file and create mapping
+
+            for i in range(0, data.shape[0], self.cut_interval - self.overlap):
+                data_mapping.append((ogg_file, audio_file_index, i))
+
+        self.data_mapping = data_mapping
--- tests/test_mp3.py
+++ tests/test_mp3.py
@@ -1,20 +1,19 @@
 import unittest
 import audio2hdf5.importer
 
-
 """
 Positive testing - I know it is bad, but I do not want to spent time with negative test cases for now :-)
 """
-class Mp3TestSet(unittest.TestCase):
 
+
+class Mp3TestSet(unittest.TestCase):
     def setUp(self):
         print("SETUP!")
         self.test_directory = "/home/hanz/tmp/sounds/accents/"
-        self.importer = audio2hdf5.importer.Importer(self.test_directory)
+        self.importer = audio2hdf5.importer.Importer(self.test_directory, overlap=22050)
 
     def teardown(self):
         print("TEAR DOWN!")
-
 
     def test_mp3(self):
         print(self.importer.get_mp3_files())
@@ -32,8 +31,10 @@ class Mp3TestSet(unittest.TestCase):
         self.assertIsNotNone(data)
 
     def test_name_to_index(self):
-        sample_names = [("english1.mp3", 1), ("english22.mp3", 22), ("english001.mp3", 1)]
+        sample_names = [("english1.ogg", 1), ("english22.ogg", 22), ("english001.ogg", 1)]
         for (name, idx) in sample_names:
             self.assertEqual(self.importer.parse_index(name), idx)
 
-
+    def test_initialize(self):
+        self.importer.initialize()
+        self.assertTrue(self.importer.is_initialized())
